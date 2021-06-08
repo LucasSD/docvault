a07b4e9746412e1552837b5b43e7a4bc197b76a1
LucasSD <lucas.stonedrake@gmail.com>
2021-06-08 12:42:22 +0100

    LegalDoc model finished and fully tested

--- coding-exercise-main/nomio/documents/models.py
+++ coding-exercise-main/nomio/documents/models.py
@@ -1,5 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
+
 
 class LegalDoc(models.Model):
      doc = models.FileField(upload_to='')
      up_date = models.DateField(auto_now_add=True)
+     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+     def __str__(self):
+          return " ".join([self.doc.name, str(self.user), str(self.up_date)])
