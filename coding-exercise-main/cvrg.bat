e8bbb36ea4b4bb1f16e31f37717d2f9c2104aa6a
LucasSD <lucas.stonedrake@gmail.com>
2021-06-12 15:26:28 +0100

    modify cvrg.bat so that test_settings.py is used. Other aesthetic changes to templates

--- coding-exercise-main/cvrg.bat
+++ coding-exercise-main/cvrg.bat
@@ -1,4 +1,4 @@
 set -e  # Configure shell so that if one command fails, it exits
 coverage erase
-coverage run manage.py test
+coverage run manage.py test --settings=nomio.test_settings
 coverage report
