826466c097b1e39357ace28d3baf0c5567ee46d1
LucasSD <lucas.stonedrake@gmail.com>
2021-08-19 18:47:54 +0100

    change secret key stuff

--- coding-exercise-main/docvault/dev_settings.py
+++ coding-exercise-main/docvault/dev_settings.py
@@ -1,3 +1,8 @@
 from .settings import *
 
 ALLOWED_HOSTS += ['*']
+
+SECRET_KEY = "dev"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
--- coding-exercise-main/docvault/test_settings.py
+++ coding-exercise-main/docvault/test_settings.py
@@ -2,3 +2,8 @@ from .settings import *
 
 # avoid accessing file system
 DEFAULT_FILE_STORAGE = "inmemorystorage.InMemoryStorage"
+
+SECRET_KEY = "dev"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
