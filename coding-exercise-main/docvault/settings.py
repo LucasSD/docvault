fe07aaa21e5b9465ef74c9185b6c6f9c84e90fa6
LucasSD <lucas.stonedrake@gmail.com>
2021-08-19 17:55:22 +0100

    collect static files for admin

--- coding-exercise-main/docvault/settings.py
+++ coding-exercise-main/docvault/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = "static"
 STATICFILES_DIRS = [BASE_DIR / "shared/static"]
 
 MEDIA_ROOT = BASE_DIR / "media"
