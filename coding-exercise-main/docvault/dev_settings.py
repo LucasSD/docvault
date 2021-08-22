927f669f33d806b578dbfa903915df90962b3164
LucasSD <lucas.stonedrake@gmail.com>
2021-08-22 11:19:33 +0100

    change to sendinblue email server

--- coding-exercise-main/docvault/dev_settings.py
+++ coding-exercise-main/docvault/dev_settings.py
@@ -152,7 +152,7 @@ MEDIA_URL = "/media/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_HOST = "smtp-relay.sendinblue.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
