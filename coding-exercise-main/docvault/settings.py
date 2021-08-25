1f3f86fba21b4a16dbbaab9d9fc5907c4d30e6b9
LucasSD <lucas.stonedrake@gmail.com>
2021-08-25 12:54:28 +0100

    add to ALLOWED_HOSTS to enable switching URLS between AWS EB environments

--- coding-exercise-main/docvault/settings.py
+++ coding-exercise-main/docvault/settings.py
@@ -37,7 +37,8 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["docvault37.eba-8bqt95tj.us-west-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["docvault37.eba-8bqt95tj.us-west-2.elasticbeanstalk.com",
+"docvault.us-west-2.elasticbeanstalk.com"]
 
 # Application definition
 
